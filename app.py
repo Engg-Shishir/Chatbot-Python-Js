975b4cf9ef3c6d6ad9618913d1358d712bcd679f
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2024-01-27 11:14:19 +0600

    Deploy in live server (render)

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-# @app.get("/")
-# def index_get():
-#     return render_template("base.html")
-
-
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
@@ -19,6 +14,3 @@ def predict():
     message = {"answer": response}
     return jsonify(message)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
